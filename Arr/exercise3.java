4c85c4469da6588742d54d2a5b1650a6b993cdc6
Gokul200318 <165988049+Gokul200318@users.noreply.github.com>
2025-02-12 21:21:58 +0530

    Test if an array contains a specific value - changed

--- Arr/exercise3.java
+++ Arr/exercise3.java
@@ -25,8 +25,12 @@ public class exercise3{
         }
         System.out.println("Enter the specific element");
         int element = scan.nextInt();
+        int element_2 = scan.nextInt();
 
         boolean result = (cont(arr,element));
         System.out.println("array contain a specific value so" +" " + result);
+
+        boolean result_2 = (cont(arr,element_2));
+        System.out.println("array contain a specific value so" +" " + result_2);
     }
 }
